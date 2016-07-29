e7c75487289dbdac3b2a0d1d49b38094090cd249
James Ide <ide@exp.host>
2016-07-29 07:10:02 +0000

    Don't run electron-rebuild during CI
    fbshipit-source-id: 93e7697

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,7 @@ let tasks = {
   ...buildTasks,
 };
 
+gulp.task('build:deploy', tasks.babel);
 gulp.task('build', gulp.parallel(tasks.buildNativeModules, tasks.babel));
 gulp.task('watch', gulp.parallel(
   tasks.buildNativeModules,
